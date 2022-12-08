1b4c9a755be946fdb6a144c51044fdc14e43660e
Ahmad-mufied <teranggelap01@gmail.com>
2022-12-08 11:33:04 +0700

    Getting started with the problem

--- main.go
+++ main.go
@@ -1,1 +1,82 @@
 package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+// The Dining Philosophers problem is well known in computer science circles.
+// Five philosopers, numbered from 0 through 4, live in a house where the
+// table is laid for them; each philosoper has their own place at the table.
+// Their only difficulty - besides  those of philosophy - is that the dish
+// served is a very difficulty kind of spaghetti which has to be eaten with
+// two forks. There are two forks next to each plate, so that presents no
+// difficulty. As a consequence, however, this means that no two neighbours
+// my be eating simultaneously, since there are five philosophers and five forks.
+//
+// This is a simple implementation of Dijkstra's solution to the "Dining
+// Philosophers" dilemma.
+
+// Philoshopher is a struct which stores information about a philosopher.
+type Philosopher struct {
+	name string
+	rightFork int
+	leftFork int
+}
+
+// philosophers is list of all philosophers.
+var philosopers = []Philosopher{
+	{name: "Plato", leftFork: 4, rightFork: 0},
+	{name: "Socrates", leftFork: 0, rightFork: 1},
+	{name: "Aristotle", leftFork: 1, rightFork: 2},
+	{name: "Pascal", leftFork: 2, rightFork: 3},
+	{name: "Locke", leftFork: 3, rightFork: 4},
+}
+
+// define some variables
+var hunger = 3 // how many times does a person eat?
+var eatTime = 1 * time.Second
+var thinkTime = 3 * time.Second
+var sleepTime = 1 * time.Second
+
+
+func main() {
+	// Print out a welcome message
+	fmt.Println("Dining Philosophers Problem")
+	fmt.Println("---------------------------")
+	fmt.Println("The table is empty.")
+
+
+	// start the meal
+	dine()
+
+	// print out finished message
+	fmt.Println("The table is empty.")
+}
+
+func dine() {
+	wg := &sync.WaitGroup{}
+	wg.Add(len(philosopers))
+
+	seated := &sync.WaitGroup{}
+	seated.Add(len(philosopers))
+
+	// forks is a map off all 5 forks.
+	var forks = make(map[int]*sync.Mutex)
+	for i := 0; i < len(philosopers); i++ {
+		forks[i] = &sync.Mutex{}
+	}
+
+	// start the meal.
+	for i := 0; i < len(philosopers); i++ {
+		// fire off a goroutine for the current philosopher
+		go diningProblem(philosopers[i], wg, forks, seated)
+	}
+
+	wg.Wait()
+}
+
+func diningProblem(philosopher Philosopher, wg *sync.WaitGroup, forks map[int]*sync.Mutex, seated *sync.WaitGroup) {
+	defer wg.Done()
+}
